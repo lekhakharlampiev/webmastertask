534ea8804bd0fb82c9cabf94f930294865380d4b
Aleksey <xarlamlexa1502@gmail.com>
2019-06-16 23:20:31 +0300

    Занялся валидацией формы

--- js/index.js
+++ js/index.js
@@ -7,29 +7,39 @@ for (var i = 0; i < promoListItem.length; i++) {
     promoListItem[i].addEventListener('click', function (evt) {
         evt.preventDefault();
         modalPopup.classList.add('popup-show');
-    })
+    });
 }
+
 closeButtonPopup.addEventListener('click', function (evt) {
     evt.preventDefault();
     modalPopup.classList.remove('popup-show');
-})
+});
+
 cancelButtonPopup.addEventListener('click', function (evt) {
     evt.preventDefault();
     modalPopup.classList.remove('popup-show');
-})
+});
+
 document.addEventListener('keydown', function (evt) {
-    evt.preventDefault();
     if (evt.keyCode === 27) {
        modalPopup.classList.remove('popup-show'); 
     }
-})
-
-var postContentCard = document.querySelectorAll('.js-post-content');
+});
 var postContentCardToggle = document.querySelectorAll('.js-post-content-show');
 
 for (var i = 0; i < postContentCardToggle.length; i++) {
     postContentCardToggle[i].addEventListener('click', function (evt) {
-        evt.preventDefault;
-        this.parentNode.classList.toggle('up-down-content');
+        evt.preventDefault();
+        this.parentElement.classList.toggle('up-down-content');
     })    
 }
+var feedbackForm = document.querySelector('.feedback-form');
+var mailInput = document.querySelector('.js-feedback-email');
+var nameUserInput = document.querySelector('.js-feedback-name');
+var selectCountryInput = document.querySelector('.js-select-input');
+var feedbackCheckbox = document.querySelector('.js-checkbox-agree');
+var feedbackSubmit = document.querySelector('.js-button-fedback-submit');
+
+mailInput.addEventListener('input', function () {
+    nameUserInput.value = this.value;
+})
