4737b5104dacdff75225e609c82ae2fd04e1b102
Aleksey <kharlampiev@aspect-systems.ru>
2019-06-17 12:37:03 +0300

    Завершил основную часть валидации формы

--- js/index.js
+++ js/index.js
@@ -41,22 +41,59 @@ var feedbackCheckbox = feedbackForm.querySelector('.js-checkbox-agree');
 var feedbackSubmit = feedbackForm.querySelector('.js-button-fedback-submit');
 var errorMail = feedbackForm.querySelector('.js-email-invalid-error');
 var errorName = feedbackForm.querySelector('.js-name-invalid-error');
+var feedbackCheckboxError = feedbackForm.querySelector('.js-feedback-checkbox');
+var countrySelectedInput = feedbackForm.querySelector('.js-select-input');
+var countryItem = feedbackForm.querySelectorAll('.cuontry-item');
+var pattermail = /^[a-zA-Z @.]+$/;
+var pattername = /^[a-zA-Z]+$/;
 
 mailInput.addEventListener('input', function () {
-    var mailvalue = this.value;
-    var pattermail = /^[a-zA-Z @]+$/;
     if (!pattermail.test(mailInput.value)) {
         errorMail.classList.add('error-show');
+        mailInput.classList.add('error');
     } else {
+        mailInput.classList.remove('error');
         errorMail.classList.remove('error-show');
     }
 })
 
 nameUserInput.addEventListener('input', function () {
-    var pattername = /^[a-zA-Z @]+$/;
     if (!pattername.test(nameUserInput.value)) {
         errorName.classList.add('error-show');
+        nameUserInput.classList.add('error');
     } else {
+        nameUserInput.classList.remove('error');
         errorName.classList.remove('error-show');
+    }
+})
+
+for (var i = 0; i < countryItem.length; i++) {
+    countryItem[i].addEventListener('click', function () {
+        countrySelectedInput.value = this.innerText;
+})
+}
+
+feedbackForm.addEventListener('submit', function (evt) {
+    if (!pattermail.test(mailInput.value)) {
+        errorMail.classList.add('error-show');
+        evt.preventDefault();
+    }
+    if (!pattername.test(nameUserInput.value)) {
+        errorName.classList.add('error-show');
+        evt.preventDefault();
+    }
+    if (!feedbackCheckbox.checked) {
+        feedbackCheckboxError.classList.add('checkbox-agree-error');
+        evt.preventDefault();
+    }
+    if (!countrySelectedInput.value) {
+        countrySelectedInput.classList.add('error');
+        evt.preventDefault();
+    }
+    if (pattermail.test(mailInput.value) && pattername.test(nameUserInput.value) && feedbackCheckbox.checked && countrySelectedInput.value) {
+        errorMail.classList.remove('error-show');
+        errorName.classList.remove('error-show');
+        feedbackCheckboxError.classList.remove('checkbox-agree-error');
+        countrySelectedInput.classList.remove('error');
     }
 })
